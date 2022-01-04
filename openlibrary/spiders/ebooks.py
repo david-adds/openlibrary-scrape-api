17df1185d7c39b5bae254d6ee8c97892eadf9dad
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-04 14:08:25 -0300

    start project

---
+++ openlibrary/spiders/ebooks.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class EbooksSpider(scrapy.Spider):
+    name = 'ebooks'
+    allowed_domains = ['openlibrary.org/subjects/picture_books.json?limit=12&offset=12']
+    start_urls = ['http://openlibrary.org/subjects/picture_books.json?limit=12&offset=12/']
+
+    def parse(self, response):
+        pass
